589964e6ec0491642f37f82ed24824215a2a5462
Mathias R. Jessen <mathias@mrjessen.com>
2019-11-14 00:43:46 +0100

    Update tests

--- test/Legislator.Tests.ps1
+++ test/Legislator.Tests.ps1
@@ -8,7 +8,7 @@ if (!$SuppressImportModule) {
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifestPath
-        $? | Should Be $true
+        $? | Should -BeTrue
     }
 }
 
@@ -18,19 +18,34 @@ Describe 'interface' {
             interface "ITest$((New-Guid) -replace '\W')" {
                 method int IsEmpty ([string])
             }
-        } |Should Not Throw
+        } |Should -Not -Throw
     }
 
     It 'Emits no output by default' {
         interface "ITest$((New-Guid) -replace '\W')" {
 
-        } |Should BeNullOrEmpty
+        } |Should -BeNullOrEmpty
     }
 
     It 'Emits the resulting interface type when PassThru is present' {
         $iName = "ITest$((New-Guid) -replace '\W')"
         interface $iName {
 
-        } -PassThru |Should BeOfType System.Type
+        } -PassThru |Should -BeOfType System.Type
+    }
+
+    It 'Produces valid interface types' {
+        $iName = "ITest$((New-Guid) -replace '\W')"
+        $iType = interface $iName {
+            property string A
+        } -PassThru
+
+        $iType |Should -BeOfType System.Type
+
+        $iType.IsInterface |Should -BeTrue
+
+        $iProp = $iType.GetProperty('A')
+
+        $iProp.PropertyType.FullName |Should -Be System.String
     }
 }
